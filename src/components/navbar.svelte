4e6463008bf321b5e16b758112fc328c8ab35008
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-02-23 12:35:05 +0530

    feat: added navbar

---
+++ src/components/navbar.svelte
@@ -0,0 +1,9 @@
+<nav class="sticky top-0 py-4 shadow-lg w-full bg-slate-800">
+  <div class="desktop:max-w-[72rem] mx-auto flex flex-row">
+    <a
+      class="px-4 py-2 font-bold desktop:text-xl text-base rounded hover:bg-slate-700 text-white transition-all"
+      href="/"
+      >Pranjal Gupta
+    </a>
+  </div>
+</nav>
