a1fa91953ed7566a7e2c9bfbb83606c5c718c41e
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-02-23 10:27:04 +0530

    feat: added search bar features (+tags)

--- src/components/indexBlogPosts.svelte
+++ src/components/indexBlogPosts.svelte
@@ -8,12 +8,51 @@
     };
     url: string;
   }[];
-  export let IndexTitle: string;
+  let tags: string[] = [
+    ...new Set(
+      posts
+        .map((post) => post.frontmatter.tags)
+        .flat()
+        .map((tag) => tag)
+    ),
+  ];
+  let selectedTags: string[] = [];
+  let searched: string = "";
+  $: filteredPosts = posts.filter((post) => {
+    return (
+      (post.frontmatter.title.toLowerCase().includes(searched.toLowerCase()) &&
+        post.frontmatter.tags.some((tag) => selectedTags.includes(tag))) ||
+      (post.frontmatter.title.toLowerCase().includes(searched.toLowerCase()) &&
+        selectedTags.length === 0) ||
+      (post.frontmatter.tags.some((tag) => selectedTags.includes(tag)) &&
+        searched === "")
+    );
+  });
 </script>
 
 <div class="flex flex-col gap-4">
-  <h1 class="font-semibold desktop:text-4xl text-2xl">{IndexTitle}</h1>
-  {#each posts as post}
+  <h1 class="font-semibold desktop:text-4xl text-2xl">All Posts</h1>
+  <input
+    placeholder="Search all posts..."
+    class="w-full px-1 py-0.5 bg-slate-700 rounded-md"
+    bind:value={searched}
+  />
+  <div class="flex flex-row gap-2 flex-wrap">
+    {#each tags as tag}
+      <button
+        class={"bg-slate-600 text-base font-semibold rounded px-1 py-0.5 " +
+          (selectedTags.includes(tag) ? "text-blue-400" : "text-neutral-400")}
+        on:click={() => {
+          selectedTags.includes(tag)
+            ? (selectedTags = selectedTags.filter((t) => t !== tag))
+            : (selectedTags = [...selectedTags, tag]);
+        }}
+      >
+        {tag}
+      </button>
+    {/each}
+  </div>
+  {#each filteredPosts as post}
     <div>
       <article class="p-4 bg-slate-700 rounded-md flex flex-col gap-1">
         <h1 class="font-semibold desktop:text-xl text-lg">
@@ -25,7 +64,9 @@
         </h1>
         <div class="flex flex-row gap-2 flex-wrap">
           {#each post.frontmatter.tags as tag}
-            <span class="bg-slate-600 font-semibold rounded px-1 py-0.5 ">
+            <span
+              class="bg-slate-600 text-base font-semibold rounded px-1 py-0.5 "
+            >
               {tag}
             </span>
           {/each}
