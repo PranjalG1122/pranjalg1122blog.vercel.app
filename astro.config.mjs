31c8dc8f487ca35f072e2ec90202cc21b9c182cd
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-07-27 14:14:20 +0530

    style: changed style to match portfolio site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
@@ -7,7 +7,15 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), mdx()],
+  integrations: [
+    svelte(),
+    tailwind(),
+    mdx({
+      shikiConfig: {
+        theme: "css-variables",
+      },
+    }),
+  ],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -1,11 +1,11 @@
 <nav
-  class="flex flex-row items-center justify-between sticky top-0 py-2 shadow-lg w-full bg-slate-950 border-b border-slate-800"
+  class="flex flex-row items-center justify-between sticky top-0 py-2 shadow-lg w-full bg-neutral-900 border-b border-neutral-700"
 >
   <div
-    class="desktop:max-w-3xl w-full items-center justify-between mx-auto flex flex-row desktop:px-0 px-4"
+    class="md:max-w-3xl w-full items-center justify-between mx-auto flex flex-row md:px-0 px-4"
   >
     <a
-      class="py-1 font-semibold desktop:text-xl text-base text-white focus:outline-none"
+      class="py-1 px-2 font-medium md:text-xl text-base text-white focus:outline-none"
       href="/"
       title="Pranjal Gupta"
       >Pranjal Gupta
