8fe36fe75e169a682c83257ce0e26b5f5b8e73ed
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-07-28 09:25:01 +0530

    style: mobile padding

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -2,7 +2,7 @@
   class="flex flex-row items-center justify-between sticky top-0 py-2 shadow-lg w-full bg-neutral-900 border-b border-neutral-700"
 >
   <div
-    class="md:max-w-3xl w-full items-center justify-between mx-auto flex flex-row md:px-0 px-4"
+    class="md:max-w-3xl w-full items-center justify-between mx-auto flex flex-row md:px-0 px-2"
   >
     <a
       class="py-1 px-2 font-medium md:text-xl text-base text-white focus:outline-none"
