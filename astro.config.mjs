76494e8bdc2fddd42686d7d13450cedac7640c15
Pranjal Gupta <95481637+PranjalG420@users.noreply.github.com>
2023-07-31 14:47:22 +0530

    fix: site url config

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://localhost:3000",
+  site: "https://pranjalg420blog.vercel.app/",
   integrations: [svelte(), tailwind(), mdx()],
   markdown: {
     shikiConfig: {
